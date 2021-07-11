4e6eec1342fc47c155dafa18a7221de36d1d6b1a
LanangAkira <77384326+LanangAkira@users.noreply.github.com>
2021-07-11 17:23:55 +0700

    Update update.py

--- update.py
+++ update.py
@@ -1,5 +1,5 @@
 import os
-print("\n\033[32;1mUpdating Wordlist...\n\n\033[0m")
-os.system('mv wordlist.txt wordlist_lama.txt')
-os.system('wget https://raw.githubusercontent.com/LanangAkira/OJS-ShellFinder/main/wordlist.txt --no-check-certificate')
-print("\033[32;1mUpdate Wordlist Success\n\033[0m")
+print("\n\033[32;1mUpdating Script...\n\n\033[0m")
+os.system('mv ojs.py ojs.py')
+os.system('wget https://raw.githubusercontent.com/LanangAkira/OJS-ShellFinder/main/ojs.py --no-check-certificate')
+print("\033[32;1mUpdate Script Success\n\033[0m")
